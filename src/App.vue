f73013d6be3cdbb5d037a729942e20490d8b0b3b
github_username <you@example.com>
2023-05-09 18:23:06 +0200

    header

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      :class="{ 'onScroll': !view.topOfPage}"
+      >
+      <div class="d-flex align-center">
+        <v-btn href="#" target="_blank" text   >
+         <h1>
+          <v-icon>mdi-food</v-icon>
+          restoran
+         </h1>
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>    
+    <!--icon button  -->
+      <v-btn   class="nav-menu" @click="tooo"> <v-icon>mdi-view-list</v-icon></v-btn>
+    <!-- nav list  -->
+    
+        <div v-for='item in items' :key='item.id'  class="nav-list"   >
+          <v-menu open-on-hover  >
+            <template v-slot:activator="{ on }" >
+                <v-btn class="nav-btn"
+                  v-on="on" 
+                > 
+                  {{item.name}}
+                  <v-icon>{{item.icon}}</v-icon>
+                </v-btn>
+            </template>
+            <v-list v-for='element in item.value ' :key='element.id' >
+              <v-list-item>
+                <v-list-item-title v-if="item.type = Array"> 
+                  <v-btn>{{ element.ex }}</v-btn>
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    items:[
+    {name:'Home',value:'' ,type:'text',icon:''},
+    {name:'About',value:'',type:'text',icon:''},
+    {name:'service',value:'',type:'text',icon:''},
+    {name:'menu',value:'',type:'text',icon:''},
+    {name:'pages',value:[
+      {ex:'booking'},
+      {ex:'our-team'},
+      {ex:'testimonial'}],type:'Array',icon:'mdi-menu-down'}
+      
+    ],
+
+    view: {
+        topOfPage: true
+      },
+    
+      X:false
+
+    }),
+  
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods:{
+    
+    handleScroll(){
+      if(window.pageYOffset>20){
+        if(this.view.topOfPage) this.view.topOfPage = false
+      } else {
+        if(!this.view.topOfPage) this.view.topOfPage = true
+      }
+    },
+    tooo(){
+      this.X =!this.X
+      console.log(this.X)
+    }
+  }
+  
+};
+
+</script>
+<style scoped lang="scss">
+.styly{
+  background: khaki;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-hero">
+        <v-container>
+           <v-row cols="6">
+              <v-col>
+                 <h1>
+                    Enjoy Our Delicious Meal
+                 </h1>
+                 <p>
+                    Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
+                 </p>
+                 <v-btn class="general-btn">
+                    Book A Table
+                 </v-btn>
+              </v-col>
+              <v-col>
+                <img src="../assets/img/hero.png">
+              </v-col>
+           </v-row>
+        </v-container>
+    </div>
+</template>
+<script>
+    export default {
+    name: 'Home',
+
+
+    }
+</script>
+<style scoped lang="scss">
+</style>
